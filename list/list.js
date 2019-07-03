a1830a7c8cf2aecc4589e0847065d156cd2d8a9b
Jonny Graybill <jon.a.graybill@gmail.com>
2019-07-02 19:54:34 -0700

    first few done

--- list/list.js
+++ list/list.js
@@ -28,6 +28,53 @@ class List {
     return returnValue;
   }
 
+  shift() {
+    if (! this.data[0]) { return undefined;}
+    let item = this.data[0];
+    delete this.data[0];
+    this.reindex();
+    return item;
+  }
+
+  unshift(arr, item) {
+    for (let i = arr.length -1; i >= 0; i--) {
+      arr[i +1] = arr[i];
+    }
+    arr[0] = item;
+  }
+
+  forEach(callback) {
+    if (this.length) {
+      for (let i = 0; i <= this.length -1; i++) {
+        callback(this[i], i);
+      }
+    }
+  }
+
+  map() {
+    
+
+  }
+
+  filter (callback) {
+    if (! this.length) {return undefined;}
+    let result = new List();
+    for (let i = 0; i <= this.length -1; i++) {
+      if (callback(this[i])) {
+        result.push(this[i]);
+      }
+    }
+    return result;
+  }
+
+  reduce(callback, state) {
+    if (! this.length) {return undefined;} 
+    for ( let i = 0; i <= this.length -1; i++) {
+      state = callback(state,this[i], i);
+    }
+    return state;
+  }
+
 }
 
 module.exports = List;
